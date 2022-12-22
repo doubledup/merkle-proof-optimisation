2b62ffafe1fb7ee6ee26665b77c1c74c0d163e6c
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-12-22 12:39:17 +0400

    Print basic Merkle tree

--- test/data/src/index.ts
+++ test/data/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world!");
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import prettier from "prettier";
+
+// (1)
+const values = [
+  ["0x1111111111111111111111111111111111111111", "5000000000000000000"],
+  ["0x2222222222222222222222222222222222222222", "2500000000000000000"],
+];
+
+// (2)
+const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
+
+// (3)
+console.log("Merkle Root:", tree.root);
+
+// (4)
+console.log(prettier.format(JSON.stringify(tree.dump()), { parser: "json" }));
